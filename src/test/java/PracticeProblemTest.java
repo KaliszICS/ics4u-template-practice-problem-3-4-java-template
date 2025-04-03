d0419ba123f90dd91f1f7c0f4f10406898c3a169
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-04-02 21:39:31 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,49 +1,120 @@
+import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.io.*;
-
 public class PracticeProblemTest {
+@Test
+@DisplayName("")
+void recamanTest6() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {int.class};
+        Method method = testClass.getDeclaredMethod("recaman", cArg);
+  // Enter code here
+    assertArrayEquals(new int[]{}, ( int[])method.invoke(null, -5));
+}
 
-   @Test
-   public void testOutput()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
 
-     // action
-     PracticeProblem.q1();
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void recamanTest5() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {int.class};
+        Method method = testClass.getDeclaredMethod("recaman", cArg);
+  // Enter code here
+    assertArrayEquals(new int[]{1, 3, 6, 2, 7, 13, 20, 12, 21, 11, 22, 10, 23, 9, 24, 8, 25, 43, 62, 42}, ( int[])method.invoke(null, 20));
+}
 
-     // assertion
-     assertEquals("There once was a man from St. Ives.\n", bos.toString());
 
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void recamanTest4() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {int.class};
+        Method method = testClass.getDeclaredMethod("recaman", cArg);
+  // Enter code here
+    assertArrayEquals(new int[]{1, 3, 6, 2, 7, 13, 20, 12, 21, 11, 22, 10, 23, 9, 24, 8, 25}, ( int[])method.invoke(null, 17));
+}
 
-   @Test
-   public void testInputandOutput()
-   {
-      String data = "Users Input";
-      System.setIn(new ByteArrayInputStream(data.getBytes()));
-      
-      PrintStream originalOut = System.out;
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(bos));
 
-      // action
-      PracticeProblem.q1();
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void recamanTest3() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {int.class};
+        Method method = testClass.getDeclaredMethod("recaman", cArg);
+  // Enter code here
+    assertArrayEquals(new int[]{}, ( int[])method.invoke(null, 0));
+}
 
-      // assertion
-      assertEquals("There once was a man from St. Ives.\n", bos.toString());
 
-      // undo the binding in System
-      System.setOut(originalOut);
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void recamanTest2() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {int.class};
+        Method method = testClass.getDeclaredMethod("recaman", cArg);
+  // Enter code here
+    assertArrayEquals(new int[]{1}, ( int[])method.invoke(null, 1));
+}
 
-   @Test
-   public void testQ3()
-   {
-     
-   }
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void recamanTest1() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {int.class};
+        Method method = testClass.getDeclaredMethod("recaman", cArg);
+  // Enter code here
+    assertArrayEquals(new int[]{1, 3, 6, 2, 7, 13}, ( int[])method.invoke(null, 6));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
 }
